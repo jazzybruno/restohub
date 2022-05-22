297f1a67d0b38f2b9f07cf67ce6d1b97fcf5f196
Jazzy Bruno <jazzybruno45@gmail.com>
2022-05-22 11:06:19 +0200

    Added conditions

--- src/components/profile/restaurant.jsx
+++ src/components/profile/restaurant.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './restaurant.css'
 import NavBar from '../Dashboard/navbar';
-// import Info from './info';
-// import RestType from './restype';
+import Info from './info';
+import RestType from './restype';
 import RestMenu from './ownmenu';
 
 const Profile = ()=>{
+    const [step, setStep]=useState(1)
+
     return(
         <div className='mainProfile h-[100%]'>
             <NavBar />
             <div className="profilerun flex ">
                 <div className="elementProfile">
                    <p className='font-bold pt-8 text-center text-2xl'>Create Your Restaurant Profile</p>
-                   <div className="options flex ml-10 mt-10 cursor-pointer">
+                   <div 
+                   onClick={()=> setStep(1)}
+                   className="options flex ml-10 mt-10 cursor-pointer">
                        <div className="numberProfile mr-10"><p className='text-center' >1</p></div>
                        <div className="descProfile">
                            <p className='font-bold text-xl'>Restaurant information</p>
@@ -20,7 +24,9 @@ const Profile = ()=>{
                        </div>
                    </div>
 
-                   <div className="options flex ml-10 mt-10 cursor-pointer">
+                   <div
+                   onClick={()=> setStep(2)}
+                    className="options flex ml-10 mt-10 cursor-pointer">
                        <div className="numberProfile mr-10"><p className='text-center' >2</p></div>
                        <div className="descProfile">
                            <p className='font-bold text-xl'>Restaurant Type and timing</p>
@@ -28,7 +34,9 @@ const Profile = ()=>{
                        </div>
                    </div>
 
-                   <div className="options flex ml-10 mt-10 cursor-pointer">
+                   <div 
+                   onClick={()=> setStep(3)}
+                   className="options flex ml-10 mt-10 cursor-pointer">
                        <div className="numberProfile mr-10"> <p className='text-center' >3</p> </div>
                        <div className="descProfile">
                            <p className='font-bold text-xl'>Create your own menu</p>
@@ -37,7 +45,9 @@ const Profile = ()=>{
                    </div>
                 </div>
                <div className='filling'>
-               <RestMenu />
+               {step === 1 && <Info />}
+               {step === 2 && <RestType />}
+               {step === 3 && <RestMenu />}
                </div>
             </div>
         </div>
