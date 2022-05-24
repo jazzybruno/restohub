d3508afe1af681b7f429dae1c3be9366fb5f99a4
hyguette-murinda <97804723+hyguette-murinda@users.noreply.github.com>
2022-05-24 09:08:14 -0700

    added responsiveness
    added responsiveness

--- src/components/forms/LogIn.js
+++ src/components/forms/LogIn.js
@@ -4,21 +4,21 @@ function LogIn() {
   return (
     <div id="bgLogIn">
       <div classname="logo">
-        <h1 className="text-6xl text-white  font-[bold] pl-[300px]">
+        <h1 className="text-6xl text-white  font-[bold] pl-[300px] md:display-none">
           Resto<span className="text-[#B3C10F]">Hub</span>
         </h1>
       </div>
       <div className=" bgForm  ">
         <div className="flexStuff">
           <h4 className="font-bold text-center">Welcome</h4>
-          <h2 className="login pb-2 pt-2 text-xl mx-20 w-[100%]">
+          <h2 className=" text-center  p-2 text-2xl mx-14 sm:text-5 ">
             Log In to Resto<span>Hub</span>
           </h2>
         </div>
-        <form className=" mt-10" action="/overview">
+        <form className=" mt-10" action="/overview" >
           <label className="login p-5">Email</label> <br />
           <input
-            className=" login1 border-2 mx-24 px-5 text-xl py-5 mb-[30px] mt-[10px] w-[73%] h-[50px]"
+            className=" login1 px-5  py-[10px] mb-[5px] mt-2 text-xl  mx-10 border-2 md:w-[400px] sm:w-[30px] h-[50px] "
             name="fname"
             placeholder="Email Adress"
           />{" "}
@@ -26,14 +26,14 @@ function LogIn() {
           <label className="login p-5">Password</label>
           <br />
           <input
-            className="login1 px-5  py-[20px] mb-[30px] mt-10 text-xl  mx-24 mt-[10px] border-2 w-[73%] h-[50px]"
+            className="login1 px-5  py-[10px] mb-[5px] mt-2 text-xl  mx-10 border-2 md:w-[400px] sm:w-[30px] h-[50px] "
             name="fname"
             placeholder="Enter your Password"
           />
           <div className=" flex justify-center content-center  ">
             <input
               type="submit"
-              className="bg-[#B3C10F] ml-10  mt-10 text-white text-2xl my-[30px]  w-[73%] h-[50px]"
+              className="bg-[#B3C10F]  mt-10 text-white text-2xl my-[30px]  md:w-[400px] sm:w-[50px] h-[50px] "
               value="Log In"
             />
           </div>
