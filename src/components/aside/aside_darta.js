d0eb703b5f8ae53bbb37e01597ff50dbcfef79a8
kevine <ishimwekevine098@gmail.com>
2022-05-27 11:17:06 +0200

    Adding the side bar

---
+++ src/components/aside/aside_darta.js
@@ -0,0 +1,25 @@
+const data =[
+    {
+        image:"./img/icon1.png",
+        name:"Overview"
+    },
+    {
+        image:"./img/icon2.png",
+        name:"Client"
+    },
+    {
+        image:"./img/icon5.png",
+        name:"Users"
+    },
+    {
+        image:"./img/icon3.png",
+        name:"Setting"
+    },
+    {
+        image:"./img/user.png",
+        name:"My Account"
+    }
+
+]
+
+export default data;
