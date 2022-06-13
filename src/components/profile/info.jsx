f59b9c0a906bffff48df80b02f24caf64c6e56b1
pacy001 <pacifiquemurangwa001@gmail.com>
2022-06-13 09:24:46 +0200

    profile forms were modified

--- src/components/profile/info.jsx
+++ src/components/profile/info.jsx
@@ -3,16 +3,18 @@ import './restaurant.css'
 
 const Info = ()=>{
         return(
-            <div className='infoMain'>
-               <form action="" className='ml-10 pt-5'>
-                   <label  htmlFor="">Restaurant Information</label>
+            <div className='ownMenu' id='res-info-container'>
+               <form action="" className='res-info'>
+                   <div>
+                   <label className='res-info-label'  htmlFor="">Restaurant Information</label>
                    <input className='longOnes' type="text" name="" id="" placeholder='Restaurant name' />
                    <input className='longOnes' type="text" name="" id="" placeholder='Restaurant complete name' />
-                   <label className='contact' htmlFor="">Contact number @ Restaurant</label>
+                   <label className='res-info-label' htmlFor="">Contact number @ Restaurant</label>
                    <input className='longOnes' type="text" name="" id="" placeholder='+250       mobile number' />
                    <div className="flex gap-24">
                        <input className='shortOnes' type="text" name="" id="" placeholder='owner name' />
                        <input className='shortOnes' type="text" name="" id="" placeholder='Restaurant email name' />
+                   </div>
                    </div>
                </form>
             </div>
