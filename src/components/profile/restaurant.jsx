2058721b937c6dd3b1edd4a898e88da6da55adbb
Hyguette Murinda <hyguettemurindaimfura@gmail.com>
2022-06-13 09:12:39 +0200

    updated
    updated

--- src/components/profile/restaurant.jsx
+++ src/components/profile/restaurant.jsx
@@ -17,7 +17,7 @@ const Profile = ()=>{
                    <p className='font-bold pt-8 text-center text-2xl'>Create Your Restaurant Profile</p>
                    
                 <div onClick={()=> setStep(1)} style={{backgroundColor:step===1 ? "#b3c10f": "white" ,width:"100%", height:"10vh", alignContent:"center", paddingLeft:"7px", marginLeft:"0px"}}
-                   className="options flex ml-10 mt-10 cursor-pointer">
+                   className="options flex ml-10 mt-5 cursor-pointer">
                        <div className="numberProfile mr-10" style={{backgroundColor:step===1 ? "white": "#b3c10f" }} ><p className='text-center' >1</p></div>
                        <div className="descProfile">
                            <p className='font-bold text-xl'>Restaurant information</p>
@@ -27,7 +27,7 @@ const Profile = ()=>{
 
                    <div
                    onClick={()=> setStep(2)} style={{backgroundColor:step===2 ? "#b3c10f": "white" ,width:"100%", height:"10vh", alignContent:"center", paddingLeft:"7px", marginLeft:"0px"}}
-                    className="options flex ml-10 mt-10 cursor-pointer ">
+                    className="options flex ml-10 mt-5 cursor-pointer ">
                        <div className="numberProfile mr-10" id='ste2'style={{backgroundColor:step===2 ? "white": "#b3c10f" }} ><p className='text-center' >2</p></div>
                        <div className="descProfile">
                            <p className='font-bold text-xl'>Restaurant Type and timing</p>
@@ -37,7 +37,7 @@ const Profile = ()=>{
 
                    <div 
                    onClick={()=> setStep(3)} style={{backgroundColor:step===3 ? "#b3c10f": "white" ,width:"100%", height:"10vh", alignContent:"center", paddingLeft:"7px", marginLeft:"0px"}}
-                   className="options flex ml-10 mt-10 cursor-pointer">
+                   className="options flex ml-10 mt-5 cursor-pointer">
                        <div className="numberProfile mr-10" style={{backgroundColor:step===3 ? "white": "#b3c10f" }}> <p className='text-center' >3</p> </div>
                        <div className="descProfile">
                            <p className='font-bold text-xl'>Create your own menu</p>
