8b3798b04f0b3e09da44e9f60e1c957d60ac522f
Hyguette Murinda <hyguettemurindaimfura@gmail.com>
2022-06-13 08:18:30 +0200

    updated restaurant.jsx
    updated restaurant.jsx

--- src/components/profile/restaurant.jsx
+++ src/components/profile/restaurant.jsx
@@ -8,16 +8,17 @@ import RestMenu from './ownmenu';
 const Profile = ()=>{
     const [step, setStep]=useState(1)
 
+
     return(
         <div className='mainProfile h-[100%]'>
             <NavBar />
             <div className="profilerun flex ">
                 <div className="elementProfile">
                    <p className='font-bold pt-8 text-center text-2xl'>Create Your Restaurant Profile</p>
-                   <div 
-                   onClick={()=> setStep(1)}
+                   
+                <div onClick={()=> setStep(1)} style={{backgroundColor:step===1 ? "#b3c10f": "white" }}
                    className="options flex ml-10 mt-10 cursor-pointer">
-                       <div className="numberProfile mr-10"><p className='text-center' >1</p></div>
+                       <div className="numberProfile mr-10" style={{backgroundColor:step===1 ? "white": "#b3c10f" }} ><p className='text-center' >1</p></div>
                        <div className="descProfile">
                            <p className='font-bold text-xl'>Restaurant information</p>
                            <p className='text-lg'>restaurant name and address</p>
@@ -25,9 +26,9 @@ const Profile = ()=>{
                    </div>
 
                    <div
-                   onClick={()=> setStep(2)}
-                    className="options flex ml-10 mt-10 cursor-pointer">
-                       <div className="numberProfile mr-10"><p className='text-center' >2</p></div>
+                   onClick={()=> setStep(2)} style={{backgroundColor:step===2 ? "#b3c10f": "white" }}
+                    className="options flex ml-10 mt-10 cursor-pointer ">
+                       <div className="numberProfile mr-10" id='ste2'style={{backgroundColor:step===2 ? "white": "#b3c10f" }} ><p className='text-center' >2</p></div>
                        <div className="descProfile">
                            <p className='font-bold text-xl'>Restaurant Type and timing</p>
                            <p className='text-lg'>establishment and cuisine type</p>
@@ -35,15 +36,16 @@ const Profile = ()=>{
                    </div>
 
                    <div 
-                   onClick={()=> setStep(3)}
+                   onClick={()=> setStep(3)} style={{backgroundColor:step===3 ? "#b3c10f": "white" }}
                    className="options flex ml-10 mt-10 cursor-pointer">
-                       <div className="numberProfile mr-10"> <p className='text-center' >3</p> </div>
+                       <div className="numberProfile mr-10" style={{backgroundColor:step===3 ? "white": "#b3c10f" }}> <p className='text-center' >3</p> </div>
                        <div className="descProfile">
                            <p className='font-bold text-xl'>Create your own menu</p>
                            <p className='text-lg'>menu resurant food images</p>
                        </div>
                    </div>
                 </div>
+                
                <div className='filling'>
                {step === 1 && <Info />}
                {step === 2 && <RestType />}
